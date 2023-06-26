97d34e42b7d873560424ad2d941e9d206ee615c4
Rahil Khatri <rahilkhatri4@gmail.com>
2023-06-25 20:03:54 -0400

    Updated header for active links and connected all pages together

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -2,23 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayTeamPage = exports.DisplayContactPage = exports.DisplayAppPage = exports.DisplayFeaturesPage = exports.DisplayHomePage = void 0;
 function DisplayHomePage(req, res, next) {
-    res.render('index');
+    res.render('index', { page: 'home' });
 }
 exports.DisplayHomePage = DisplayHomePage;
 function DisplayFeaturesPage(req, res, next) {
-    res.render('features');
+    res.render('features', { page: 'features' });
 }
 exports.DisplayFeaturesPage = DisplayFeaturesPage;
 function DisplayAppPage(req, res, next) {
-    res.render('app');
+    res.render('app', { page: 'app' });
 }
 exports.DisplayAppPage = DisplayAppPage;
 function DisplayContactPage(req, res, next) {
-    res.render('contact');
+    res.render('contact', { page: 'contact' });
 }
 exports.DisplayContactPage = DisplayContactPage;
 function DisplayTeamPage(req, res, next) {
-    res.render('team');
+    res.render('team', { page: 'team' });
 }
 exports.DisplayTeamPage = DisplayTeamPage;
 //# sourceMappingURL=index.js.map
--- Server/Controllers/index.ts
+++ Server/Controllers/index.ts
@@ -3,29 +3,29 @@ import express from 'express';
 //Controller logic to display your home page
 export function DisplayHomePage(req: express.Request , res: express.Response , next: express.NextFunction): void
 {
-    res.render('index');
+    res.render('index' , {page : 'home'});
 }
 
 //Controller logic to display your services page
 export function DisplayFeaturesPage(req: express.Request , res: express.Response , next: express.NextFunction): void
 {
-    res.render('features');
+    res.render('features' , {page : 'features'});
 }
 
 //Controller logic to display your projects page
 export function DisplayAppPage(req: express.Request , res: express.Response , next: express.NextFunction): void
 {
-    res.render('app');
+    res.render('app' , {page : 'app'});
 }
 
 //Controller logic to display your contact page
 export function DisplayContactPage(req: express.Request , res: express.Response , next: express.NextFunction): void
 {
-    res.render('contact');
+    res.render('contact' , {page : 'contact'});
 }
 
 //Controller logic to display your team page
 export function DisplayTeamPage(req: express.Request , res: express.Response , next: express.NextFunction): void
 {
-    res.render('team');
+    res.render('team' , {page : 'team'});
 }
